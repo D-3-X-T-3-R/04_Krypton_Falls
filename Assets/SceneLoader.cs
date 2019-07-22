4164a455cbefebae2ef7e8129cc0fbbe8f45031e
D-3-X-T-3-R <43900202+D-3-X-T-3-R@users.noreply.github.com>
2019-07-23 00:51:20 +0530

    Re-Added assets. Added splash screen. Added intro music

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadNextScene", 5f);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
